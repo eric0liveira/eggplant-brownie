181aa99135279ded1782005257d8ba352bd6d7c4
Eric Rodrigues <eric0liveira@Erics-MacBook-Pro.local>
2017-12-17 23:05:41 -0200

    Initial Commit

---
+++ eggplant-brownie/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  eggplant-brownie
+//
+//  Created by Eric de Olivera Rodrigues on 17/12/17.
+//  Copyright © 2017 Eric de Olivera Rodrigues. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
