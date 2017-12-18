3ec11f274aac76e6797eafef7d0a5d00d85e5ef8
Eric de Olivera Rodrigues <eric0liveira@Erics-MacBook-Pro.local>
2017-12-18 00:05:45 -0200

    Introdução

--- eggplant-brownie/ViewController.swift
+++ eggplant-brownie/ViewController.swift
@@ -9,17 +9,15 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
+    @IBOutlet var nameField : UITextField!
+    @IBOutlet var happinessField : UITextField!
+    
+    @IBAction func add(){
+        let name = nameField.text
+        let happiness = happinessField.text
+        print("eaten \(name) with happiness \(happiness)!")
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 
 }
 
