6c271d749a760dac156c30d7b13b47f575c77a74
Eric de Olivera Rodrigues <eric.erikito@gmail.com>
2017-12-18 19:34:25 -0200

    Funções com e sem parâmetros

---
+++ eggplant-brownie/MyPlayground.playground/Contents.swift
@@ -0,0 +1,23 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+let name:String = "eggplant"
+let calories:Double = 50.5
+let happiness:Int = 5
+let eggplantIsAVegetable:Bool = false
+
+func helloCalories(){
+    let name = "product"
+    print("hello calories \(name)")
+}
+
+helloCalories()
+print(name)
+
+func add(name:String, calories:Double){
+    print("adding \(name) with \(calories)")
+}
+
+add(name: "brownie", calories: 30.5)
+add(name: "pao", calories: 50.3)
