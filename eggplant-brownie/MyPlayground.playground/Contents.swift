b5a8bd5bd4eb6b91740b1db7bc8dca400d7928c2
Eric de Olivera Rodrigues <eric.erikito@gmail.com>
2017-12-20 20:06:01 -0200

    Métodos que retornam opcionais

--- eggplant-brownie/MyPlayground.playground/Contents.swift
+++ eggplant-brownie/MyPlayground.playground/Contents.swift
@@ -1,48 +1,23 @@
-//: Playground - noun: a place where people can play
-
 import UIKit
 
-let name:String = "eggplant"
-let calorie:Double = 50.5
-let happiness:Int = 5
-let eggplantIsAVegetable:Bool = false
-
-func helloCalories(){
-    let name = "product"
-    print("hello calories \(name)")
+class Meal{
+    var happiness:Int = 5
+    var name:String = "eggplant brownie"
 }
 
-helloCalories()
+let brownie = Meal()
+print(brownie.happiness)
+print(brownie.name)
+
+brownie.happiness = 4
+
+print(brownie.happiness)
+
+var name:String?
+name = "eggplant brownie"
 print(name)
+print(name!.uppercased())
 
-func add(name:String, calories:Double){
-    print("adding \(name) with \(calories)")
+if let name = name {
+    print(name.uppercased())
 }
-
-add(name: "brownie", calories: 30.5)
-add(name: "pao", calories: 50.3)
-
-let calories = [50.5, 100, 30]
-
-for i in 0...2 {
-    print(calories[i])
-}
-
-for i in 0...(calories.count - 1) {
-    print(calories[i])
-}
-
-for calorie in calories{
-    print(calorie)
-}
-
-func allCalories(calories:Array<Double>) -> Double {
-    var total = 0.0
-    for cal in calories {
-        total += cal
-    }
-    
-    return total
-}
-
-print(allCalories(calories: calories))
