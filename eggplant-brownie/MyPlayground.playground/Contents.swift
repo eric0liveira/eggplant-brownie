a4aafe1599c41f9810dbd08f5f68f5390fddd722
Eric de Olivera Rodrigues <eric.erikito@gmail.com>
2017-12-19 22:02:29 -0200

    Métodos que retornam opcionais

--- eggplant-brownie/MyPlayground.playground/Contents.swift
+++ eggplant-brownie/MyPlayground.playground/Contents.swift
@@ -3,7 +3,7 @@
 import UIKit
 
 let name:String = "eggplant"
-let calories:Double = 50.5
+let calorie:Double = 50.5
 let happiness:Int = 5
 let eggplantIsAVegetable:Bool = false
 
@@ -21,3 +21,28 @@ func add(name:String, calories:Double){
 
 add(name: "brownie", calories: 30.5)
 add(name: "pao", calories: 50.3)
+
+let calories = [50.5, 100, 30]
+
+for i in 0...2 {
+    print(calories[i])
+}
+
+for i in 0...(calories.count - 1) {
+    print(calories[i])
+}
+
+for calorie in calories{
+    print(calorie)
+}
+
+func allCalories(calories:Array<Double>) -> Double {
+    var total = 0.0
+    for cal in calories {
+        total += cal
+    }
+    
+    return total
+}
+
+print(allCalories(calories: calories))
