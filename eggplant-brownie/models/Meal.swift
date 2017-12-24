738a56c70eddcfbfa0f57bcbed72b28954840440
Eric de Olivera Rodrigues <eric.erikito@gmail.com>
2017-12-24 16:44:36 -0200

    Organizando o projeto com grupos

---
+++ eggplant-brownie/models/Meal.swift
@@ -0,0 +1,28 @@
+//
+//  Meal.swift
+//  eggplant-brownie
+//
+//  Created by Eric de Olivera Rodrigues (@eric0liveira) on 24/12/17.
+//  Copyright © 2017 Eric de Olivera Rodrigues (@eric0liveira). All rights reserved.
+//
+
+import Foundation
+
+class Meal {
+    let name:String
+    let happiness:Int
+    let items = Array<Item>()
+    
+    init(name:String, happiness:Int) {
+        self.name = name
+        self.happiness = happiness
+    }
+    
+    func allCalories() -> Double {
+        var total = 0.0
+        for item in items {
+            total += item.calories
+        }
+        return total
+    }
+}
---
+++ eggplant-brownie/viewcontrollers/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  eggplant-brownie
+//
+//  Created by Eric de Olivera Rodrigues (@eric0liveira) on 17/12/17.
+//  Copyright © 2017 Eric de Olivera Rodrigues (@eric0liveira). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var nameField : UITextField?
+    @IBOutlet var happinessField : UITextField?
+    
+    @IBAction func add(){
+        if (nameField == nil || happinessField == nil) {
+            return
+        }
+        let name:String = nameField!.text!
+        
+        if let happiness:Int = Int(happinessField!.text!) {
+            let meal = Meal(name: name, happiness: happiness)
+            
+            print("eaten \(meal.name) with happiness \(meal.happiness)!")
+        }
+    }
+
+}
+
