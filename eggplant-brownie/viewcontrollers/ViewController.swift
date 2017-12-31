bbc0af220ee19b0f16b321417e9b23ee84527274
Eric de Olivera Rodrigues <eric.erikito@gmail.com>
2017-12-31 00:58:17 -0200

    Funções fora de classe na hora de remover itens

--- eggplant-brownie/viewcontrollers/ViewController.swift
+++ eggplant-brownie/viewcontrollers/ViewController.swift
@@ -8,11 +8,49 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
     @IBOutlet var nameField : UITextField?
     @IBOutlet var happinessField : UITextField?
     var delegate : AddAMealDelegate?
+    var selected = Array<Item>()
+    
+    var items = [Item(name: "Eggplant", calories: 10),
+                 Item(name: "Brownie", calories: 10),
+                 Item(name: "Zucchini", calories: 10),
+                 Item(name: "Muffin", calories: 10),
+                 Item(name: "Coconut oil", calories: 500),
+                 Item(name: "Chocolate frosting", calories: 1000),
+                 Item(name: "Chocolate chip", calories: 1000)]
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let row = indexPath.row
+        let item = items[row]
+        
+        let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: nil)
+        cell.textLabel!.text = item.name
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if let cell = tableView.cellForRow(at: indexPath) {
+            if(cell.accessoryType == UITableViewCellAccessoryType.none){
+                cell.accessoryType = UITableViewCellAccessoryType.checkmark
+                let item = items[indexPath.row]
+                selected.append(item)
+            } else {
+                cell.accessoryType = UITableViewCellAccessoryType.none
+                let item = items[indexPath.row]
+                if let position = selected.index(of: item) {
+                    selected.remove(at: position)
+                }
+            }
+        }
+    }
     
     @IBAction func add(){
         if (nameField == nil || happinessField == nil) {
@@ -21,9 +59,9 @@ class ViewController: UIViewController {
         let name:String = nameField!.text!
         
         if let happiness:Int = Int(happinessField!.text!) {
-            let meal = Meal(name: name, happiness: happiness)
+            let meal = Meal(name: name, happiness: happiness, items: selected)
             
-            print("eaten \(meal.name) with happiness \(meal.happiness)!")
+            print("eaten \(meal.name) with happiness \(meal.happiness) with \(meal.items)!")
             
             if delegate == nil {
                 return
